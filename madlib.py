3fcfba5f64432641a2c46b71fa984becdf4aac92
wkinne <wkinne@fullsail.edu>
2014-07-10 12:37:04 -0400

    Input Validations for Adk

--- madlib.py
+++ madlib.py
@@ -4,8 +4,20 @@ adj_1 = raw_input("Please Enter An Adjective: ")
 
 adj_2 = raw_input("Please Enter Another Adjective: ")
 
+while adj_1 == adj_2:
+    print "Adjectives cannot be identical"
+    adj_1 = raw_input("Please Enter An Adjective: ")
+    adj_2 = raw_input("Please Enter Another Adjective: ")
+
+
+
 noun_1 = raw_input("Please Enter A Noun: ")
 noun_2 = raw_input("Please Enter Another Noun: ")
+
+while noun_1 == noun_2:
+    print "Adjectives cannot be identical"
+    adj_1 = raw_input("Please Enter An Adjective: ")
+    adj_2 = raw_input("Please Enter Another Adjective: ")
 
 fill_items = []
 
