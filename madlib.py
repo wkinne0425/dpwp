ace04622b299e1e425bf39a4222db09bc0e549ba
wkinne <wkinne@fullsail.edu>
2014-07-10 10:50:43 -0400

    first test

--- madlib.py
+++ madlib.py
@@ -1,16 +1,22 @@
 __author__ = 'wkinne'
 
 adj_1 = raw_input("Please Enter An Adjective")
+'''
 adj_2 = raw_input("Please Enter Another Adjective")
 
 noun_1 = raw_input("Please Enter A Noun")
 noun_2 = raw_input("Please Enter Another Noun")
-
+'''
 fill_items = []
 
 def addItems(a,aa,n,nn):
 
     fill_items.insert(a)
+    return fill_items
+
+test = fill_items(adj_1)
+
+print test
 
 
 
