6816012aa250c4e3287f232181e5a0feca4e6139
wkinne <wkinne@fullsail.edu>
2014-07-10 10:36:17 -0400

    Added Noun Variables

--- madlib.py
+++ madlib.py
@@ -3,3 +3,7 @@ __author__ = 'wkinne'
 adj_1 = raw_input("Please Enter An Adjective")
 adj_2 = raw_input("Please Enter Another Adjective")
 
+noun_1 = raw_input("Please Enter A Noun")
+noun_2 = raw_input("Please Enter Another Noun")
+
+
