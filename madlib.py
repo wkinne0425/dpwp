95f513cb5889ca04d59581ada1ced2ea050c4ec0
wkinne <wkinne@fullsail.edu>
2014-07-10 11:01:16 -0400

    Now have all parameters

--- madlib.py
+++ madlib.py
@@ -1,20 +1,24 @@
 __author__ = 'wkinne'
 
-adj_1 = raw_input("Please Enter An Adjective")
-'''
-adj_2 = raw_input("Please Enter Another Adjective")
+adj_1 = raw_input("Please Enter An Adjective: ")
 
-noun_1 = raw_input("Please Enter A Noun")
-noun_2 = raw_input("Please Enter Another Noun")
-'''
+adj_2 = raw_input("Please Enter Another Adjective: ")
+
+noun_1 = raw_input("Please Enter A Noun: ")
+noun_2 = raw_input("Please Enter Another Noun: ")
+
 fill_items = []
 
-def addItems(a):
+def addItems(a,aa,n,nn):
 
     fill_items.insert(0, a)
+    fill_items.insert(1, aa)
+    fill_items.insert(2, n)
+    fill_items.insert(3, nn)
+
     return fill_items
 
-test = addItems(adj_1)
+test = addItems(adj_1, adj_2, noun_1, noun_2)
 
 print test
 
