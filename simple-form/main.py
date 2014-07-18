64df374171b4c0c76954223ddb92ce589b66c4ce
wkinne <wkinne@fullsail.edu>
2014-07-17 21:03:49 -0400

    Starting project

--- simple-form/main.py
+++ simple-form/main.py
@@ -1,126 +1,10 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+
 
 
 import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        head = '''
-        <!doctype html>
-
-        <html lang="en">
-        <head>
-            <meta charset="utf-8">
-
-        <title>Simple Form</title>
-          <meta name="description" content="The HTML5 Herald">
-          <meta name="author" content="SitePoint">
-          <link rel="stylesheet" href="css/main.css" />
-
-
-
-        </head>
-        '''
-
-        body = '''
-
-        <body>
-
-               <form method="GET" action="">
-               <label>Name: </label><input type="text" name="name" />
-               <label>Email: </label><input type="text" name="email" />
-               <label>Loan Amount: </label><input type="text" name="loan" />
-
-               <div id="buttons">
-               <h3>Choose your credit:</h3>
-
-               <input type="radio" name="radio_button" value="excellent">Excellent
-               <input type="radio" name="radio_button" value="good">Good
-               <input type="radio" name="radio_button" value="fair">Fair
-               </div>
-
-
-
-               <h3> Choose your terms: </h3>
-               <select name="length"><option value="15">15yr</option><option value="30">30yr</option></select>
-
-
-               <input class="button" type="submit" value="Submit" />
-               </form>
-               '''
-
-        footer = '''
-        <footer></footer>
-        </body>
-        </html>
-
-                '''
-
-        def calc():
-
-            interest = 0
-            loan_amount = self.request.GET['loan']
-            loan_number = int(loan_amount)
-            name = self.request.GET['name']
-
-            if self.request.GET['radio_button'] == 'excellent':
-                interest = 3
-                #self.response.write(interest)
-
-            elif self.request.GET['radio_button'] == 'good':
-                interest = 5
-                #self.response.write(interest)
-
-            else:
-                interest = 8
-                #self.response.write(interest)
-
-            total_interest = (loan_number * interest)
-            number = int(total_interest)
-            final_interest = number / 100.0
-            final_interest_str = str(final_interest)
-            #self.response.write(final_interest)
-            #self.response.write("<br />")
-
-            total_loan = loan_number + final_interest
-            total_loan_str = str(total_loan)
-            #self.response.write(total_loan)
-
-            term = self.request.GET['length']
-            term_number = int(term)
-
-            monthly = total_loan / term_number / 12.0
-            monthly_str = str(monthly)
-
-            #self.response.write(monthly)
-
-
-
-
-
-
-
-
-            self.response.write("Ok " + name + " here are your loan terms: <br />" )
-            self.response.write("Loan Amount: " + "$" + loan_amount + "<br />")
-            self.response.write("Interest Amount: " + "$" + final_interest_str + "<br />")
-            self.response.write("Total Loan: " + "$" + total_loan_str + "<br />")
-            self.response.write("Monthy Payments: " + "$" + monthly_str)
 
 
 
@@ -130,10 +14,14 @@ class MainHandler(webapp2.RequestHandler):
 
 
 
-        if self.request.GET:
-            calc()
-        else:
-            self.response.write(head + body + footer)
+
+
+
+
+
+
+
+
 
 
 
