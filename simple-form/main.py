6d95c91908b6f8f5f13975eea4908020378c46e9
wkinne <wkinne@fullsail.edu>
2014-07-17 21:04:21 -0400

    adding head

--- simple-form/main.py
+++ simple-form/main.py
@@ -5,8 +5,17 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
+         head = '''
+        <!doctype html>
 
+        <html lang="en">
+        <head>
+            <meta charset="utf-8">
 
+        <title>Simple Form</title>
+          <meta name="description" content="The HTML5 Herald">
+          <meta name="author" content="SitePoint">
+          <link rel="stylesheet" href="css/main.css" />
 
 
 
