d8e846957e781d4b0c429f55d3270265b8f78971
wkinne <wkinne@fullsail.edu>
2014-07-17 21:05:17 -0400

    added body

--- simple-form/main.py
+++ simple-form/main.py
@@ -17,6 +17,36 @@ class MainHandler(webapp2.RequestHandler):
           <meta name="author" content="SitePoint">
           <link rel="stylesheet" href="css/main.css" />
 
+         </head>
+            '''
+
+        body = '''
+
+        <body>
+
+               <form method="GET" action="">
+               <label>Name: </label><input type="text" name="name" />
+               <label>Email: </label><input type="text" name="email" />
+               <label>Loan Amount: </label><input type="text" name="loan" />
+
+               <div id="buttons">
+               <h3>Choose your credit:</h3>
+
+               <input type="radio" name="radio_button" value="excellent">Excellent
+               <input type="radio" name="radio_button" value="good">Good
+               <input type="radio" name="radio_button" value="fair">Fair
+               </div>
+
+
+
+               <h3> Choose your terms: </h3>
+               <select name="length"><option value="15">15yr</option><option value="30">30yr</option></select>
+
+
+               <input class="button" type="submit" value="Submit" />
+               </form>
+               '''
+
 
 
 
