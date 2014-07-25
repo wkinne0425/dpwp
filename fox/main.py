b21f64adeafd3d5e8c226509d391796c2fa79cda
wkinne <wkinne@fullsail.edu>
2014-07-24 22:10:34 -0400

    adding more information to animals

--- fox/main.py
+++ fox/main.py
@@ -9,12 +9,12 @@ class MainHandler(webapp2.RequestHandler):
         bird = AbstractAnimal()
 
 
-        tiger.inputs = [ ("Lion", "Chordata", "Mammalia", "Carnivora", "Felidae", "Panthera tigris", "<img src='http://images.nationalgeographic.com/wpf/media-live/photos/000/007/cache/siberian-tiger_707_600x450.jpg' />", "<embed height='50' width='100' src='audio/lion.mp3'>", "<a href='?sound=lion'>Lion</a>")]
+        tiger.inputs = [ ("Lion", "Chordata", "Mammalia", "Carnivora", "Felidae", "Panthera tigris", "<img src='http://images.nationalgeographic.com/wpf/media-live/photos/000/007/cache/siberian-tiger_707_600x450.jpg' />", "<embed height='50' width='100' src='audio/lion.mp3'>", "<a href='?sound=lion'>Lion</a>", "10 - 14 Years" )]
         #stiger.inputs = [1,2,3,4,5]
         #self.response.write(tiger.print_all())
-        shark.inputs = [ ("Shark", "Chordata", "Chondrichthyes", "Pristiophoiformes ", "Lamnidae", "Isurus oxyrinchus", "<img src='http://2.bp.blogspot.com/-1acB83dcmAk/UK2X6orz4LI/AAAAAAAACDs/L_5a6wcYwxA/s1600/Silvertip+Sharks2.jpg' />", "<embed height='50' width='100' src='audio/jaw_theme.mp3'>", "<a href='?sound=shark'>Shark</a>")]
+        shark.inputs = [ ("Shark", "Chordata", "Chondrichthyes", "Pristiophoiformes ", "Lamnidae", "Isurus oxyrinchus", "<img src='http://2.bp.blogspot.com/-1acB83dcmAk/UK2X6orz4LI/AAAAAAAACDs/L_5a6wcYwxA/s1600/Silvertip+Sharks2.jpg' />", "<embed height='50' width='100' src='audio/jaw_theme.mp3'>", "<a href='?sound=shark'>Shark</a>", "20 - 30 years")]
         #self.response.write(shark.print_all())
-        bird.inputs = [ ("Bird", "Chordata", "Aves", "Passeriformes ", "Turdidae", "Turdus", "<img src='http://astromatrix.org/Content/images/Objects/Bird.jpg' />", "<embed height='50' width='100' src='audio/birds002.mp3'>", "<a href='?sound=bird'>Bird</a>")]
+        bird.inputs = [ ("Bird", "Chordata", "Aves", "Passeriformes ", "Turdidae", "Turdus", "<img src='http://astromatrix.org/Content/images/Objects/Bird.jpg' />", "<embed height='50' width='100' src='audio/birds002.mp3'>", "<a href='?sound=bird'>Bird</a>", "50 - 80 Years")]
 
 
 
@@ -95,6 +95,7 @@ class AbstractAnimal(Page):
         self.Link = ""
 
 
+
         #stores all the data in a array
         self.__inputs = []
 
@@ -110,11 +111,12 @@ class AbstractAnimal(Page):
         self.__inputs = arr
         for item in arr:
              self.Title = "<h1>" + item[0] + "</h1>"
-             self.Phylum =  "<li>" + item[1] + "</li>"
-             self.Class = "<li>" + item[2] + "</li>"
-             self.Order = "<li>" + item[3] + "</li>"
-             self.Family = "<li>" + item[4] + "</li>"
-             self.Genus = "<li>" + item[5] + "</li>"
+             self.Phylum =  "<li> Phylum: " + item[1] + "</li>"
+             self.Class = "<li> Class: " + item[2] + "</li>"
+             self.Order = "<li> Order: " + item[3] + "</li>"
+             self.Family = "<li> Family: " + item[4] + "</li>"
+             self.Genus = "<li> Genus: " + item[5] + "</li>"
+             self.Avg = "<li>Average Life: " + item[9] + "</li>"
              self.Image = "<div>" + item[6] + "</div>"
              self.Sound = "<div>" + item[7] + "</div>"
              self.Link = item[8]
@@ -127,13 +129,14 @@ class AbstractAnimal(Page):
         return self.html_close
 #prints everything inside html
     def print_all(self):
-        self.all = self.container_open +  self.Title + self.ul_open + self.Phylum + self.Class + self.Order + self.Family + self.Genus  + self.ul_close + self.Image  + self.Link + self.container_close
+        self.all = self.container_open +  self.Title + self.ul_open + self.Phylum + self.Class + self.Order + self.Family + self.Genus + self.Avg  + self.ul_close + self.Image  + self.Link + self.container_close
 
         return self.all
 #plays the sound for each bird
     def play(self):
         return self.Sound
 
+#I DID IT!!
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
