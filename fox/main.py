5bff6ac8c77897609e8e274dcd2275225b86d109
wkinne <wkinne@fullsail.edu>
2014-07-24 21:52:52 -0400

    project complete. Will go over with comments

--- fox/main.py
+++ fox/main.py
@@ -14,6 +14,8 @@ class MainHandler(webapp2.RequestHandler):
         #self.response.write(tiger.print_all())
         shark.inputs = [ ("Shark", "Chordata", "Chondrichthyes", "Pristiophoiformes ", "Lamnidae", "Isurus oxyrinchus", "<img src='http://2.bp.blogspot.com/-1acB83dcmAk/UK2X6orz4LI/AAAAAAAACDs/L_5a6wcYwxA/s1600/Silvertip+Sharks2.jpg' />", "<embed height='50' width='100' src='audio/jaw_theme.mp3'>", "<a href='?sound=shark'>Shark</a>")]
         #self.response.write(shark.print_all())
+        bird.inputs = [ ("Bird", "Chordata", "Aves", "Passeriformes ", "Turdidae", "Turdus", "<img src='http://astromatrix.org/Content/images/Objects/Bird.jpg' />", "<embed height='50' width='100' src='audio/birds002.mp3'>", "<a href='?sound=bird'>Bird</a>")]
+
 
 
 
@@ -21,7 +23,9 @@ class MainHandler(webapp2.RequestHandler):
             self.response.write(tiger.open() + shark.print_all() + shark.play() +  tiger.html_close)
         elif (self.request.GET and self.request.GET['sound'] == 'lion'):
             self.response.write(tiger.open() + tiger.print_all() + tiger.play() +  tiger.html_close)
-        else: self.response.write(tiger.open() + tiger.print_all() + shark.print_all() +  tiger.html_close )
+        elif (self.request.GET and self.request.GET['sound'] == 'bird'):
+            self.response.write(tiger.open() + bird.print_all() + bird.play() +  tiger.html_close)
+        else: self.response.write(tiger.open() + tiger.print_all() + shark.print_all() + bird.print_all() +  tiger.html_close )
 
 
 
@@ -44,6 +48,8 @@ class Page(object):
 
         self.ul_open = "<ul>"
         self.ul_close = "</ul>"
+        self.container_open = "<div class='cont'>"
+        self.container_close = "</div>"
 
         self.html_close = '''
 
@@ -120,7 +126,7 @@ class AbstractAnimal(Page):
         return self.html_close
 
     def print_all(self):
-        self.all =  self.Title + self.ul_open + self.Phylum + self.Class + self.Order + self.Family + self.Genus  + self.ul_close + self.Image  + self.Link
+        self.all = self.container_open +  self.Title + self.ul_open + self.Phylum + self.Class + self.Order + self.Family + self.Genus  + self.ul_close + self.Image  + self.Link + self.container_close
 
         return self.all
 
