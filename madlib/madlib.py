f6572cbd8a7829b40db98a13acc03ec1f0f23834
wkinne <wkinne@fullsail.edu>
2014-09-04 16:07:59 -0400

    Validating adjectives

--- madlib/madlib.py
+++ madlib/madlib.py
@@ -5,9 +5,19 @@ __author__ = 'wkinne'
 adj_1 = raw_input("Please Enter An Adjective: ")
 adj_2 = raw_input("Please Enter Another Adjective: ")
 
+#This validates that the 2 variables are not the same
+while adj_1 == adj_2:
+    print "Adjectives cannot be identical"
+    adj_1 = raw_input("Please Enter An Adjective: ")
+    adj_2 = raw_input("Please Enter Another Adjective: ")
+
+
 #Retreiving the variables from the user for the Nouns
 noun_1 = raw_input("Please Enter A Noun: ")
 noun_2 = raw_input("Please Enter Another Noun: ")
+
+
+
 
 #Retreiving numbers
 number_1 = raw_input("Please Enter a Number: ")
