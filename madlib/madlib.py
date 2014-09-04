2236d34755f7551b2e3d94f6522a7785aa87dfd5
wkinne <wkinne@fullsail.edu>
2014-09-04 16:00:29 -0400

    grabbing adjectives

--- madlib/madlib.py
+++ madlib/madlib.py
@@ -1,1 +1,6 @@
 __author__ = 'wkinne'
+
+
+#Retreiving the variables from the user for the adjectives
+adj_1 = raw_input("Please Enter An Adjective: ")
+adj_2 = raw_input("Please Enter Another Adjective: ")
