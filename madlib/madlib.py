a45238ec0b59d6f8360eab71ad314990fa0326fd
wkinne <wkinne@fullsail.edu>
2014-09-04 16:06:15 -0400

    Grabbing the numbers

--- madlib/madlib.py
+++ madlib/madlib.py
@@ -8,3 +8,8 @@ adj_2 = raw_input("Please Enter Another Adjective: ")
 #Retreiving the variables from the user for the Nouns
 noun_1 = raw_input("Please Enter A Noun: ")
 noun_2 = raw_input("Please Enter Another Noun: ")
+
+#Retreiving numbers
+number_1 = raw_input("Please Enter a Number: ")
+number_2 = raw_input("Please Enter a Number: ")
+number_3 = raw_input("Please Enter a Number: ")
