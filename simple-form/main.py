6e11a594a81a695f2f5734508e229155b4c2d6f8
wkinne <wkinne@fullsail.edu>
2014-09-11 21:11:43 -0400

    testing form

--- simple-form/main.py
+++ simple-form/main.py
@@ -2,7 +2,8 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-     head = '''
+        #header of the page
+        head = '''
         <!doctype html>
 
         <html lang="en">
@@ -18,23 +19,25 @@ class MainHandler(webapp2.RequestHandler):
 
         </head>
         '''
-
-    body = '''
+    #Body of the page
+        body = '''
 
         <body>
-        <form method="GET" action="">
+
+               <form method="GET" action="">
                <label>Name: </label><input type="text" name="name" />
                <label>Email: </label><input type="text" name="email" />
                <label>Loan Amount: </label><input type="text" name="loan" />
 
-                <div id="buttons">
+               <div id="buttons">
                <h3>Choose your credit:</h3>
 
                <input type="radio" name="radio_button" value="excellent">Excellent
                <input type="radio" name="radio_button" value="good">Good
                <input type="radio" name="radio_button" value="fair">Fair
                </div>
-               </div>
+
+
 
                <h3> Choose your terms: </h3>
                <select name="length"><option value="15">15yr</option><option value="30">30yr</option></select>
@@ -44,12 +47,14 @@ class MainHandler(webapp2.RequestHandler):
                </form>
                '''
 
-    footer = '''
+        footer = '''
         <footer></footer>
         </body>
         </html>
 
                 '''
+
+        self.response.write(head + body + footer)
 
 
 
