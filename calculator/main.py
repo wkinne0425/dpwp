b4f32d54a7323ff5951057cd477ee64e16b8fd7a
wkinne <wkinne@fullsail.edu>
2014-09-18 20:45:48 -0400

    Starting the Body class for the html elements

---
+++ calculator/main.py
@@ -0,0 +1,24 @@
+
+import webapp2
+
+class MainHandler(webapp2.RequestHandler):
+
+ class Page(object):
+    def __init__(self):
+        self.head = '''
+        <!DOCTYPE>
+            <html>
+            <head>
+            <title></title>
+            <link rel="stylesheet" href="css/main.css" />
+            </head>
+            <body>
+            <h1>Lets learn how good these players really are!</h1>
+            <h3>Click below to reveal each players scoring average for 5 rounds of golf</h3>
+        '''
+
+
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler)
+], debug=True)
