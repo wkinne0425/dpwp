fffa8ed198bec06cb74352da15f800f72eaae6a9
wkinne <wkinne@fullsail.edu>
2014-09-21 20:52:29 -0400

    added conditionals

--- api/main.py
+++ api/main.py
@@ -7,8 +7,32 @@ from xml.dom import minidom
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
+        
+        #if statement that validates the input field is not emplty and displays proper info according to ticker
 
-     self.head = '''
+        if  self.request.GET and self.request.GET["ticker"] == "":
+            self.response.write(page.print_full_open())
+            self.response.write(page.input)
+            self.response.write("Must fill in this field")
+            self.response.write(page.print_close())
+        elif self.request.GET and self.request.GET["ticker"]:
+            url.ticker = self.request.GET["ticker"]
+            self.response.write(page.print_results_open())
+            self.response.write("<h1 class='blue'>Top 3 results for " + url.ticker + "</h1>")
+            self.response.write(url.display())
+            self.response.write(page.print_close())
+        else:
+            self.response.write(page.print_full_open())
+            self.response.write(page.input)
+            self.response.write(page.print_close())
+
+
+
+
+#This creates all the page attributes for the html
+class Page(object):
+    def __init__(self):
+        self.head = '''
         <!DOCTYPE>
             <html>
             <head>
@@ -18,13 +42,13 @@ class MainHandler(webapp2.RequestHandler):
             </head>
             <body>
         '''
-     self.body = '''
+        self.body = '''
 
 
 
         '''
 
-     self.main = "<h1>Get your finance headlines</h1>" \
+        self.main = "<h1>Get your finance headlines</h1>" \
                     "<h3>Please use the field below to enter you ticker</h3>" \
                     "<p>&quot;You must enter a correct ticker or an error will appear&quot;</p>"
         self.ul_close = "</ul>"
@@ -43,9 +67,11 @@ class MainHandler(webapp2.RequestHandler):
 
         </body>
         </html>
-
-
         '''
+
+
+
+
 
     def print_full_open(self):
 
@@ -62,13 +88,13 @@ class MainHandler(webapp2.RequestHandler):
         return self.close
 
 
+#This is the Abstract Class
 class Ticker(object):
     def __init__(self):
 #Attributes that hold data for proper url
         self.url = "http://finance.yahoo.com/rss/headline?s="
         self.ticker = ""
-
-    #Method that connects url to one piece and also parse xml
+#Method that connects url to one piece and also parse xml
     def display(self):
 
         self.final = self.url + self.ticker
@@ -77,17 +103,18 @@ class Ticker(object):
         self.result = self.opener.open(self.request)
         self.xmldoc = minidom.parse(self.result)
 
+#This class inherits from Ticker
 class DisplayHeadlines(Ticker):
     def __init__(self):
         Ticker.__init__(self)
-
+#POLY at work here. Over riding the method display and adding all the functionality to it
     def display(self):
         self.final = self.url + self.ticker
         self.request = urllib2.Request(self.final)
         self.opener = urllib2.build_opener()
         self.result = self.opener.open(self.request)
         self.xmldoc = minidom.parse(self.result)
-
+#displays info from the first title and link
         self.header_1 = "<h3>" + self.xmldoc.getElementsByTagName('title')[2].firstChild.nodeValue + "</h3>"
         self.link_1 = "<a href=" +  self.xmldoc.getElementsByTagName('link')[2].firstChild.nodeValue + ">" + "Link To Story" + "</a>"
 #displays info from the second title and link
@@ -98,6 +125,7 @@ class DisplayHeadlines(Ticker):
         self.link_3 = "<a href=" +  self.xmldoc.getElementsByTagName('link')[4].firstChild.nodeValue + ">" + "Link To Story" + "</a>"
 #returns all information for display
         return self.header_1 + self.link_1 + self.header_2 + self.link_2 + self.header_3 + self.link_3 + "<a class='home' href='http://localhost:12080'>Home</a>"
+
 
 
 app = webapp2.WSGIApplication([
