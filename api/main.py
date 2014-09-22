60bff40f574c340a38c79e25b4eb026cba30a103
wkinne <wkinne@fullsail.edu>
2014-09-21 20:45:11 -0400

    body and main added

--- api/main.py
+++ api/main.py
@@ -18,7 +18,26 @@ class MainHandler(webapp2.RequestHandler):
             </head>
             <body>
         '''
+     self.body = '''
 
+
+
+        '''
+
+     self.main = "<h1>Get your finance headlines</h1>" \
+                    "<h3>Please use the field below to enter you ticker</h3>" \
+                    "<p>&quot;You must enter a correct ticker or an error will appear&quot;</p>"
+        self.ul_close = "</ul>"
+        self.container_open = "<div class='cont'>"
+        self.container_close = "</div>"
+        self.main_container_open = "<div class='main_cont'>"
+        self.main_container_close = "</div>"
+        self.input = '''
+        <form method="GET" action="">
+        <label>Ticker: </label><input type="text" name="ticker" />
+        <input class="button" type="submit" value="Submit" />
+               </form>
+        '''
 
 
 app = webapp2.WSGIApplication([
