30b0b31299338859975bb2d773182a5ec843f00f
wkinne <wkinne@fullsail.edu>
2014-09-21 20:44:31 -0400

    created head

--- api/main.py
+++ api/main.py
@@ -8,6 +8,16 @@ from xml.dom import minidom
 class MainHandler(webapp2.RequestHandler):
     def get(self):
 
+     self.head = '''
+        <!DOCTYPE>
+            <html>
+            <head>
+            <title></title>
+            <link rel="stylesheet" href="css/main.css" />
+            <link href='http://fonts.googleapis.com/css?family=Rajdhani' rel='stylesheet' type='text/css'>
+            </head>
+            <body>
+        '''
 
 
 
